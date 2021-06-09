a6d0953acf817d722defb66d86b33efb1f3cd978
Fernanda Diomede <fcdiomede@gmail.com>
2021-06-08 21:17:36 -0500

    Instantiate Post object

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -20,12 +20,12 @@ router.post('/', [ auth, [
     try {
         const user = await User.findById(req.user.id).select('-password');
 
-        const newPost = {
+        const newPost = new Post({
             text: req.body.text,
             name: user.name,
             avatar: user.avatar,
             user: req.user.id
-        }
+        })
 
         const post = await newPost.save();
 
